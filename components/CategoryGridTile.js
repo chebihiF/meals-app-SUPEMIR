83f263edf1193fe4f0b9cab427aa60706d0b0430
chebihiF <chebihi.f@ucd.ac.ma>
2022-06-09 19:55:56 +0100

    y

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { Pressable, View, Text } from "react-native";
+
+const CategoryGridTile = ({ title }) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
